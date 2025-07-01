0ff3160021fd124833a7d75453399cfae1a079d8
Marcelo Menezes <mfdemenezes@gmail.com>
2025-07-01 19:47:06 -0300

    alteração variavel

--- variables.tf
+++ variables.tf
@@ -39,3 +39,8 @@ variable "availability_zone" {
   type        = string
   default     = "us-east-1a"
 }
+variable "ami_id" {
+  description = "The AMI ID to use for the EC2 instance"
+  type        = string
+  default     = "ami-09e6f87a47903347c" # Example AMI ID, replace with your desired AMI
+}
