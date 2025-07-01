4d8fe9259010c40be0ec561cd674c098f4227159
Marcelo Menezes <mfdemenezes@gmail.com>
2025-07-01 19:50:37 -0300

    alteração variavel

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_instance" "web" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
   key_name               = var.key_name
   # ...outros parâmetros...
 }
