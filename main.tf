4528568679995b1739c49448e7becebad4d365b4
Marcelo Menezes <mfdemenezes@gmail.com>
2025-07-08 19:55:11 -0300

    ajuste outputs

--- main.tf
+++ main.tf
@@ -7,3 +7,14 @@ module "s3" {
   bucket_name = "mfdemenezes1234"
   
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "mfdemenezes-terraform"
+    key            = "global/s3/terraform.tfstate" # caminho do arquivo de estado
+    region         = "us-east-1"
+    encrypt        = true
+  
+  }
+}
+
